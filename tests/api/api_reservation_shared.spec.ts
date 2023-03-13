da8e4332dbcec3587caa857775f7b472dd4c3b25
Ernesto Quinta <equinta@MacBook-Pro.local>
2023-03-13 11:42:38 -0400

    formatting the reservation shared test

--- tests/api/api_reservation_shared.spec.ts
+++ tests/api/api_reservation_shared.spec.ts
@@ -252,17 +252,15 @@ const schema = {
 }
 
 
-test.describe.only("Api Reservation Shared", () => {
+test.describe("Api Reservation Shared", () => {
     
    test("Validate the reservation shared schema", async ({reservationEndpoints}) => {
-      //const res = await reservationEndpoints.getReservationsShared(ENV.REQUESTOR_API_KEY);
-      
-      const res = await reservationEndpoints.getReservationsShared(ENV.REQUESTOR_API_KEY, '2017-01-01');
-      const _response = JSON.parse(res);
-      console.log(`Reservation endpoint array contains ${_response.length} item(s)`);
-      await expect(_response.length).toBeGreaterThanOrEqual(1);
-      console.log('Reservation shared endpoint do not have any error - the array error have lenght '+(v.validate(_response, schema).errors).length);
-      console.log(v.validate(_response, schema));
-      await expect((v.validate(_response, schema).errors).length).toEqual(0);
+        const res = await reservationEndpoints.getReservationsShared(ENV.REQUESTOR_API_KEY, '2017-01-01');
+        const _response = JSON.parse(res);
+        console.log(`Reservation endpoint array contains ${_response.length} item(s)`);
+        await expect(_response.length).toBeGreaterThanOrEqual(1);
+        console.log('Reservation shared endpoint do not have any error - the array error have lenght '+(v.validate(_response, schema).errors).length);
+        console.log(v.validate(_response, schema));
+        await expect((v.validate(_response, schema).errors).length).toEqual(0);
    })
 })
