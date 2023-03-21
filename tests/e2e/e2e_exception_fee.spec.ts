15199cc52f6661d9a17b54c9b24acea595788a22
Jose Melendez <jmelendez@reloquest.com>
2023-03-21 14:55:43 -0400

    SM-17749 => Data update for the exception fee Test Suite

--- tests/e2e/e2e_exception_fee.spec.ts
+++ tests/e2e/e2e_exception_fee.spec.ts
@@ -2,13 +2,13 @@ import test  from '@lib/BaseTest';
 import ENV  from '@utils/env';
 
 
-test.describe.skip("Test Suite Exception Fee", () => { //serial
+test.describe("Test Suite Exception Fee", () => { //serial
     test.slow();
     const location = ["New York State, USA","Central Park North, Manhattan, New York, NY, USA","Melrose, Manhattan, NY, USA","Manhattan, New York, NY, USA","New York, NY, USA","Queens, NY, USA","Bronx, NY, USA"];
 
     for(let i = 0; i < location.length; i++){
         test(`Create a new Request via api and validate the exception fee for ${location[i]}`, async({requestEndpoints, webActions,homePage, dashboard, search, requestShow, option}) =>{
-            const _response = await requestEndpoints.createRequest(ENV.BASE_URL,'nt1reqadmin_api_key', Number(ENV.EXCEPTION_FEE_CLIENT_ID), location[i], ENV.START_DATE, ENV.END_DATE);
+            const _response = await requestEndpoints.createRequest(ENV.BASE_URL,ENV.REQUESTOR_API_KEY, Number(ENV.EXCEPTION_FEE_CLIENT_ID), location[i], ENV.START_DATE, ENV.END_DATE);
             ENV.REQUEST_ID = JSON.parse(_response).request_id;
             console.info(`Request Id: ${ENV.REQUEST_ID}`);
             await webActions.navigateTo(ENV.BASE_URL);
