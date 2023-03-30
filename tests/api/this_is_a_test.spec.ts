0574c7b7c8e4bce7755e9a5de1790c09f2a9fdb3
Ernesto Quinta <equinta@MacBook-Pro.local>
2023-03-30 16:43:54 -0400

    working to coonect mysql database

--- tests/api/this_is_a_test.spec.ts
+++ tests/api/this_is_a_test.spec.ts
@@ -9,9 +9,5 @@ test.only("testing the random", async ()=> {
     //     console.log(await WebActions.generateRandom(0,10,[8])); 
     // }
 
-    const con = await Database.dbConnection(process.env.DB_URL, process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD);
-    console.log(con);
-    const result = await Database.execute(con,`select rq_pro from smart_request where uid= "F943F8"`);
-    console.log(result);
-    //await Database.disposeDb(con);
+    
 })
