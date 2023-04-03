4afb50049d3df9d2bee37c8eb3cef24f09d6433a
Ernesto Quinta <equinta@MacBook-Pro.local>
2023-04-03 09:25:11 -0400

    working with database connection

--- lib/Database.ts
+++ lib/Database.ts
@@ -1,28 +1,34 @@
-import mysql from 'mysql';
-import {RowDataPacket} from 'mysql';
+const mysql = require('mysql2') ;
+//import {RowDataPacket} from 'mysql2';
 
 export default class Database {
     
     
-    static async dbConnection(db_host:string, db_name:string, db_username:string, db_password:string){
-        const db = mysql.createConnection({
-            host: db_host,
-            user: db_username,
-            password: db_password,
-            database: db_name
-        });
+    static async dbConnection(db_host, db_name, db_username, db_password, query){
+        //const db = mysql.createConnection(`mysql://${db_username}:${db_password}@${db_host}/${db_name}`);
+        const con = await mysql.createConnection({
+            host: `${db_host}`,
+            user: `${db_username}`,
+            password: `${db_password}`,
+            database: `${db_name}`
+        })
 
-        return db
+        // const sql = `SELECT metaValue FROM ${this.table} WHERE metaKey = '${key}'`;
+        // const result = await this.pool.query(sql);
+        // console.log(result);
+        // result.forEach(row => {
+        //     console.log(row.metaValue)
+        // })
+        // await con.promise().query({ sql: query, rowsAsArray: true }, function(err, results, fields) {
+        //     console.log(results) // will be an array of arrays rather than an array of objects
+        //     console.log(fields) // these are unchanged
+        //   });
     }
 
-    static async execute(con, query:string){
-        con.query(
-            query,
-            (err, result) => {
-              if (err) throw err;
-              const row = (<RowDataPacket> result)[0];
-              console.log(row)
-            }
-          );
+    static async execute(con, q){
+        await con.query({ sql: q, rowsAsArray: true }, function(err, results, fields) {
+            console.log(results) // will be an array of arrays rather than an array of objects
+            console.log(fields) // these are unchanged
+          });
     }
 }
--- tests/api/this_is_a_test.spec.ts
+++ tests/api/this_is_a_test.spec.ts
@@ -1,13 +1,15 @@
 import Database from "@lib/Database";
 import WebActions from "@lib/WebActions";
 import test from "@playwright/test";
+import ENV from "@utils/env";
 
-test.only("testing the random", async ()=> {
+test.skip("testing the random", async ()=> {
 
     // for (let i = 0 ; i <= 20 ; i++){
         
     //     console.log(await WebActions.generateRandom(0,10,[8])); 
     // }
 
-    
+  Database.dbConnection(ENV.DB_URL, ENV.DB_NAME, ENV.DB_USERNAME, ENV.DB_PASSWORD, 'SELECT * from `smart_request` where `uid`= "F943F8"');
+    //Database.execute(con, 'SELECT * from `smart_request` where `uid`= "F943F8"');
 })
