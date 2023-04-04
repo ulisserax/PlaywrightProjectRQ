c2b5ecac196a78f93759327409cc87895ebc16ab
Ernesto Quinta <equinta@MacBook-Pro.local>
2023-04-04 08:55:37 -0400

    removing unnecessary file

--- tests/api/this_is_a_test.spec.ts
+++ tests/api/this_is_a_test.spec.ts
@@ -3,7 +3,7 @@ import test from "@playwright/test";
 
 test.only("testing the random", async ()=> {
 
-  // const results =  await Database.execute('SELECT * FROM smart_company WHERE name = "nt1sup"');
+  
 
   // const results = await Database.execute('UPDATE smart_company SET name="nt1sup" where id = 500;');
   // console.log(results);
