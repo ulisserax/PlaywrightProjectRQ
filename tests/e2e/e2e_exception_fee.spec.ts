24c1fcb8554a5e7bd9b5a5b65af252ddf89b936f
Ernesto Quinta <equinta@MacBook-Pro.local>
2023-04-24 09:50:50 -0400

    working on exception fee

--- tests/e2e/e2e_exception_fee.spec.ts
+++ tests/e2e/e2e_exception_fee.spec.ts
@@ -2,7 +2,7 @@ import test  from '@lib/BaseTest';
 import ENV  from '@utils/env';
 
 
-test.describe.only("Test Suite Exception Fee", () => { //serial
+test.describe("Test Suite Exception Fee", () => { //serial
     test.slow();
     const location = ["New York State, USA","Central Park North, Manhattan, New York, NY, USA","Melrose, Manhattan, NY, USA","Manhattan, New York, NY, USA","New York, NY, USA","Queens, NY, USA","Bronx, NY, USA"];
 
