d3e690ff366bdcbbaec5f5597a87ccdd21b68ce6
Ernesto Quinta <equinta@MacBook-Pro.local>
2023-04-25 16:12:09 -0400

    headless true for all the config

--- e2e.config.ts
+++ e2e.config.ts
@@ -22,7 +22,7 @@ const config : PlaywrightTestConfig = {
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter:[['html', {open: "never"}]],
     use: {
-        headless: false,
+        headless: true,
         viewport:{width:1280, height:720},
         actionTimeout:100000,
         ignoreHTTPSErrors:true,
