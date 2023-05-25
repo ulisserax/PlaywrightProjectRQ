0ec4034733623743186935c273af10119a0f8973
Ernesto Quinta <equinta@MacBook-Pro.local>
2023-05-25 17:19:49 -0400

    testing commit

--- tests/api/tc_to_test_methods.spec.ts
+++ tests/api/tc_to_test_methods.spec.ts
@@ -6,6 +6,9 @@ test("Test case for test purpose", async ()=> {
   // const results = await Database.execute('UPDATE smart_company SET name="nt1sup" where id = 500;');
   // console.log(results);
 
+  // const results = await Database.execute('UPDATE smart_company SET name="nt1sup" where id = 500;');
+  // console.log(results);
+
   const result =  await Database.execute('SELECT id,name,sub_domain,default_company_name FROM smart_company WHERE name = "nt3reqrqpro"');
   console.log(result);
 
